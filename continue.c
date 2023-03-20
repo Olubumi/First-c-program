2a2d418c20c282947217233c1a19f1a4dd42e71b
Isaac Mesole <105315868+Olubumi@users.noreply.github.com>
2023-03-20 17:12:41 +0100

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,25 @@
+// Simple C program to display "Hello World"
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    
+    int a = 0, b = 1, c= 2, d = 3, e = 4;
+    a = b - c + d * e;
+
+  /* learning C programming  the answer is  11 */
+   
+    printf("%d" , a);
+
+    /* just geting started*/
+  
+    return '0';
+
+    
+}
